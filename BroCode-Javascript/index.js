58ef51e69e68d60244f946a6a1d9e328538c9b8c
Sabira1iyev <s.aliyev2005@gmail.com>
2025-12-12 11:11:50 -0500

    textContent

---
+++ BroCode-Javascript/index.js
@@ -0,0 +1,13 @@
+console.log(`Hello`);
+
+//window.alert("this is an alert");
+//window.alert(`i like pizza`);
+
+
+document.getElementById("myH1").textContent = `hello`;
+document.getElementById("myP").textContent =   `i like pizza`;
+
+// const myName = document.getElementById("myName");
+// myName.innerHTML = "<p><b>SABIR ALIYEV</b></p>"
+// console.log(myName)
+
---
+++ BroCode-Javascript/veriables.js
@@ -0,0 +1,25 @@
+// let x = 100;
+
+// console.log(x);
+
+// let age = 25;
+// let price = 1321;
+// let gpa = 2.1;
+// let name = "Sabir";
+
+// console.log(typeof(name));
+// console.log(`Your name is ${name}`)
+// console.log(typeof(gpa))
+// console.log(`you are ${age} old`);
+// console.log(`the price is ${price}`);
+// console.log(`your gap is ${gpa}`);
+
+
+let fullName = "Sabir Aliyev";
+let age = 20;
+let student = true;
+
+document.getElementById("p1").textContent = `your name is ${fullName}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `enrolled ${student}`;
+
