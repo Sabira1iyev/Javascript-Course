d0b0185569dc8a4ad239f868999753741a8006ce
Sabira1iyev <s.aliyev2005@gmail.com>
2025-12-19 07:06:35 -0500

    random generator

---
+++ BroCode-Javascript/9-RandomNumber.js
@@ -0,0 +1,30 @@
+
+// const min = 50;
+// const max = 100;
+
+// let randomNumber = Math.floor(Math.random() * (max -min)) + min; 
+
+// console.log(randomNumber);
+
+const myButton = document.getElementById("myButton");
+const myLabel1 = document.getElementById("label1");
+const myLabel2 = document.getElementById("label2");
+const myLabel3 = document.getElementById("label3");
+const min = 1;
+const max = 6
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 =  Math.floor(Math.random() * max) + min;
+    randomNum2 =  Math.floor(Math.random() * max) + min;
+    randomNum3 =  Math.floor(Math.random() * max) + min;
+
+    myLabel1.textContent = randomNum1;
+    myLabel2.textContent = randomNum2;
+    myLabel3.textContent = randomNum3;
+}
+
+
