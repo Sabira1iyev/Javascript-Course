45c40d84700f1acfc42aa425c0a3fd21e0145a9a
Sabira1iyev <s.aliyev2005@gmail.com>
2025-12-30 12:58:39 -0500

    Switch

---
+++ BroCode-Javascript/14-StringMethods.js
@@ -0,0 +1,23 @@
+
+let userName =  " Aliyevdv";
+
+
+// console.log(userName.charAt(0)); => o indexde hansi herfin oldugunu gosterir
+
+// console.log(userName.indexOf("i")); => i-nin hansi indexde oldugunu gosterir
+
+// console.log(userName.lastIndexOf("v")); =>  girilen herfin en sondan necenci indexde oldugunu gosterir
+
+// console.log(userName.length); sozun uzunlugunu verir
+
+// userName = userName.trim() => onden arxadan ola bilecek bosluqlari silir
+// console.log(userName);
+
+// userName = userName.toUpperCase(); => sozdeki herflerin hamisini boyuk herfe cevirir
+
+// userName = userName.toLowerCase(); => sozdeki herflerin hamisini kicik herfe cevirir
+
+// userName = userName.repeat(3);  => girilen say qeder stringi tekrar tekrar yazir
+
+let result = userName.startsWith(" ");
+console.log(result);
