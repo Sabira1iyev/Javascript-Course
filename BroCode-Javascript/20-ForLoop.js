3a61cbe417531cc420059df985e95fab643eb786
Sabira1iyev <s.aliyev2005@gmail.com>
2026-01-22 15:32:11 -0500

    forLoop

---
+++ BroCode-Javascript/20-ForLoop.js
@@ -0,0 +1,14 @@
+// fOR loop = repeat some code a LIMITED amount of times
+
+// for (let i = 0; i <= 10; i+=2) {
+//   console.log(i);
+// }
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    continue;
+  } 
+  else {
+    console.log(i);
+  }
+}
