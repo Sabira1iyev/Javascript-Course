9d51c5ac2af28e7018318005160e7214e99c969a
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-05 00:06:17 +0400

    veriable-scope

---
+++ BroCode-Javascript/22-Functions.js
@@ -0,0 +1,20 @@
+
+
+
+function add(x, y) {
+    let result = x + y;
+    return result;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+
+function isEven(number) {
+    return number % 2 === 0 ? true : false;
+}
+
+console.log(isEven(1));
+
+console.log(subtract(2, 3));
---
+++ BroCode-Javascript/23-VeriableScope.js
@@ -0,0 +1,16 @@
+
+
+let x = 3;
+
+function1();
+function2();
+
+function function1(){
+    let x = 1;
+    console.log(x);
+}
+
+function function2(){
+    let x = 2;
+    console.log(x);
+}
