b528e3e76f63e2d1b4da097ce0590f3af75715e2
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-08 16:52:21 +0400

    arrays

---
+++ BroCode-Javascript/25-Arrays.js
@@ -0,0 +1,18 @@
+
+
+let fruits = ["apple", "orange", "banana"];
+
+// for(let i = 0; i< fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+// fruits.sort().reverse();
+
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+
