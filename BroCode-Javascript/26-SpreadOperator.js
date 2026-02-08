6a79d2b2fe6b2382d33670708f6d3b59ec3fb04e
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-08 22:08:10 +0400

    new commit

---
+++ BroCode-Javascript/26-SpreadOperator.js
@@ -0,0 +1,20 @@
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+// console.log(minimum);
+// console.log(maximum);
+
+
+// let username = "sabir aliyev";
+// let letters = [...username].join("-");
+
+// console.log(letters);
+
+
+
+// let fruits = ["apple", "orange", "banana"];
+// let vegetables = ["carrots", "celery", "potatoes"];
+// let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+
+// console.log(foods);
